4ab8769f75b0e018cf864d9bc4f6c735a160986e
rkb.lenin <rkb.lenin@gmail.com>
2020-11-08 22:20:41 -0500

    integracion de for

--- part1/clase1.java
+++ part1/clase1.java
@@ -4,4 +4,10 @@ public class clase1{
 			System.out.println(" clase1 xd");
 		}
 	}
+
+	public static void main(String[] args) {
+		for (int i = 0 ; i <= 10 ; i++ ) {
+			System.out.println(" clase1_1 xd");
+		}
+	}
 }
